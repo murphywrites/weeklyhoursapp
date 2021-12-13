3880ed1311fe63d11dadcad852da7897fa21278c
murphywrites <mike@murphyscode.com>
2021-12-13 10:53:40 -0800

    cc

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ app.post('/', (req,res) => {
   }
 
   const challengeResponse = () => {
-    console.log("running challenge response", res.token)
+    console.log("running challenge response", res.body)
     res.writeHead(200, {'Content-Type': 'application/json'})
     res.end(`{"challenge":"${challenge}"`)
   }
